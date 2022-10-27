e9efe25262b786e875699094aad4183189171100
bernard <bvdv9075@gmail.com>
2022-10-27 10:40:59 +0100

    added initial data

--- budget/models.py
+++ budget/models.py
@@ -11,24 +11,15 @@ class Period(models.Model):
         return self.month
 
 
-# class Category(models.Model):
-#     DEFAULT = "Home"
-#     CATEGORIES = [
-#         ("Home", "Home"),
-#         ("School", "School"),
-#         ("Utility", "Utilities"),
-#         ("Groceries", "Groceries"),
-#         ("Mobile", "Mobile"),
-#         ("Subscriptions", "Subscriptions"),
-#         ('Car', "Car"),
-#         ("Other", "Other"),
-#     ]
-#
-#     # month = models.ForeignKey(Period, on_delete=models.CASCADE)
-#     category = models.CharField(max_length=20)
-#
-#     def __str__(self):
-#         return self.category
+class Category(models.Model):
+    category = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.category
+
 
 class Income(models.Model):
     person = models.CharField(max_length=180, unique=False)
@@ -43,37 +34,34 @@ class Income(models.Model):
 
 
 class Items(models.Model):
-    DEFAULT = "Home"
-    CATEGORIES = [
-        ("Home", "Home"),
-        ("School", "School"),
-        ("Utility", "Utilities"),
-        ("Groceries", "Groceries"),
-        ("Data", "Data"),
-        ("Subscriptions", "Subscriptions"),
-        ('Car', "Car"),
-        ("Other", "Other"),
-    ]
-
-    class Meta:
-        verbose_name_plural = "items"
-
     month = models.ForeignKey(Period, on_delete=models.CASCADE)
     name = models.CharField(max_length=180, unique=True)
     value = models.CharField(max_length=20)
-    category = models.CharField(max_length=20, choices=CATEGORIES,
-                                default=DEFAULT)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     payment_date = models.DateField()
     person = models.ForeignKey(Income, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "items"
 
     def __str__(self):
         return f'{self.month.month} - {self.name}'
 
 
 class BaseConfig(models.Model):
+    COUNTRIES = [
+        ("South Africa", "South Africa"),
+        ("United Kingdom", "United Kingdom"),
+    ]
+    DEFAULT = "United Kingdom"
+
     name = models.CharField(max_length=100, unique=True)
     currency = models.CharField(max_length=10, unique=True)
-    country = models.CharField(max_length=100, unique=True)
+    country = models.CharField(
+        max_length=50,
+        choices=COUNTRIES,
+        default=DEFAULT
+    )
 
     class Meta:
         verbose_name_plural = "base config"
